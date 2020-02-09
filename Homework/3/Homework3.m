5b69ffdeaa31cdbc986e2c29ed176b9cddd247fc
Damien Prieur <dap789@live.com>
2020-02-08 22:37:54 -0500

    Homework-3: Added initial files and attempted theory

---
+++ Homework/3/Homework3.m
@@ -0,0 +1,14 @@
+%{
+    Damien Prieur
+    CS 435
+    Assignment 3
+%}
+
+%% Global Setup
+
+
+output_location_prefix = 'images/generated/';
+%cleanup any previously generated images
+delete('images/generated/*');
+fprintf('Cleaned "images/generated"\n');
+
